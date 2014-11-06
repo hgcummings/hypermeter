4b175f2cbf4fa9d3b63d2817b94ca3926716f244
hgcummings <harry@xlr3.com>
2014-11-06 23:28:48 +0000

    Close process when done (resolves #1)

--- timeline.js
+++ timeline.js
@@ -1,12 +1,15 @@
 var config = require('./config.json');
 var http = require('http');
+var Q = require('q');
+var HTTP = require("q-io/http");
 
+var requests = [];
 config.urls.forEach(function(url) {
-  var start = process.hrtime();
-  http.get(url, function(res) {
-    var diff = process.hrtime(start);
-    console.log(url + ' took %d milliseconds', diff[0] * 1e3 + diff[1] / 1e6);
-  }).on('error', function(e) {
-    console.log("Got error: " + e.message);
-  });
+    var start = process.hrtime();
+    requests.push(HTTP.request(url).then(function() {
+        var diff = process.hrtime(start);
+        console.log(url + ' took %d milliseconds', diff[0] * 1e3 + diff[1] / 1e6);
+    }));
 });
+
+Q.all(requests).then(process.exit);
