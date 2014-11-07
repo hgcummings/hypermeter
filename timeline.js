fc4105dfa5670ddb80cc14912e0f0e3aabb2d760
hgcummings <harry@xlr3.com>
2014-11-07 20:15:32 +0000

    Record failures and retur non-zero exit code

--- timeline.js
+++ timeline.js
@@ -4,13 +4,32 @@ var Q = require('q');
 var HTTP = require("q-io/http");
 
 var requests = [];
+var failures = [];
 config.urls.forEach(function(url) {
     var start = process.hrtime();
-    requests.push(HTTP.request(url).then(function(response) {
-        var diff = process.hrtime(start);
-        console.log(url + ' returned %s, took %d milliseconds',
-            response.status, diff[0] * 1e3 + Math.round(diff[1] / 1e6));
-    }));
+    requests.push(HTTP.request(url)
+        .then(function(response) {
+            var diff = process.hrtime(start);
+            console.log(url + ' returned %s, took %d milliseconds',
+                response.status, diff[0] * 1e3 + Math.round(diff[1] / 1e6));
+            if (parseInt(response.status, 10) >= 400) {
+                failures.push(url);
+            }
+        })
+        .fail(function(err) {
+            console.log(err);
+            failures.push(url);
+        }));
 });
 
-Q.all(requests).then(process.exit);
+Q.all(requests).then(function() {
+    if (failures.length > 0) {
+        console.log('FAILED URLs:')
+        console.log(failures);
+    }
+    process.exit(failures.length);
+}).fail(function(err) {
+    console.log('Error');
+    console.log(err);
+    process.exit(1);
+});
