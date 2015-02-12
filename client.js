7c5303a117d4fffc8181a86a46d5150fa2ce6ab8
unknown <HGC@pineten.zoo.lan>
2015-02-12 16:53:51 +0000

    Speculative fix for cURL response issue that occurs on some environments

--- client.js
+++ client.js
@@ -14,7 +14,7 @@ exports.create = function(config) {
             }
             extend(options, config);
             return Q.nfcall(curl.request, options).then(function(curlOutput) {
-                var stdout = curlOutput[0];
+                var stdout = curlOutput[curlOutput.length-1];
                 return {
                     status: stdout.substr(stdout.lastIndexOf('#') + 1)
                 }
